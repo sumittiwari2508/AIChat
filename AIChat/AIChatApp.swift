fc316d5e174f275a11e43a2a01392d69171c6985
Sumit Kumar Tiwari <59642140+sumittiwari2508@users.noreply.github.com>
2026-07-05 15:23:28 +0530

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by $umit on 05/07/26.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
